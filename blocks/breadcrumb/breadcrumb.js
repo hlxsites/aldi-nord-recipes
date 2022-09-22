33f2743844e9ec76696a5c417755ebfdaad1f2d8
Mirko Sagolj <msagolj@adobe.com>
2022-09-22 17:17:42 +0200

    lint issues

--- blocks/breadcrumb/breadcrumb.js
+++ blocks/breadcrumb/breadcrumb.js
@@ -1,51 +1,50 @@
 import { decorateIcons } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-    block.textContent = '';
+  block.textContent = '';
 
-    let paths = document.location.pathname.split('/');
-    let ul = document.createElement('ul');
+  let paths = document.location.pathname.split('/');
+  let ul = document.createElement('ul');
+  // DEMO: we have no homepage, rezepte is the root page
+  paths.shift();
+  paths[0] = 'rezepte';
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+  a.append('Startseite');
+  a.setAttribute('href', '/');
+  li.append(a);
+  ul.append(li);
 
-    // DEMO: we have no homepage, rezepte is the root page
-    paths.shift();
-    paths[0] = "rezepte"
-    let li = document.createElement('li')
-    let a = document.createElement('a')
-    a.append("Startseite");
-    a.setAttribute("href", "/")
-    li.append(a);
+  // go through the path segments
+  let href = '';
+  paths.forEach((element, i) => {
+    let li = document.createElement('li');
+
+    // arrow icon, unless root
+
+    let span = document.createElement('span');
+    span.setAttribute('class', 'icon icon-arrow-right');
+    li.append(span);
+
+
+    // add link, if not page itself
+    if (i !== paths.length - 1) {
+      let a = document.createElement('a');
+      if (i != 0) {
+        href += "/" + element;
+        a.setAttribute('href', href + '.html');
+      } else {
+        a.setAttribute('href', '/');
+      }
+
+      a.innerText = element;
+      li.append(a);
+    } else {
+      li.append(element);
+    }
+
     ul.append(li);
-
-    // go through the path segments
-    let href = "";
-    paths.forEach((element, i) => {
-        let li = document.createElement('li');
-
-        // arrow icon, unless root
-
-        let span = document.createElement('span');
-        span.setAttribute("class", "icon icon-arrow-right");
-        li.append(span);
-
-
-        // add link, if not page itself
-        if (i !== paths.length - 1) {
-            let a = document.createElement('a');
-            if (i != 0) {
-                href += "/" + element;
-                a.setAttribute("href", href + ".html");
-            } else {
-                a.setAttribute("href", "/");
-            }
-
-            a.innerText = element;
-            li.append(a)
-        } else {
-            li.append(element)
-        }
-
-        ul.append(li);
-    });
-    block.append(ul);
-    decorateIcons(block);
+  });
+  block.append(ul);
+  decorateIcons(block);
 }
--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,7 +1,7 @@
 export default function decorate(block) {
-    // hero title is embeddd in the image itself
-    // so only take the image
-    const picture = block.querySelector('picture');
-    block.textContent = '';
-    block.append(picture);
+  // hero title is embeddd in the image itself
+  // so only take the image
+  const picture = block.querySelector('picture');
+  block.textContent = '';
+  block.append(picture);
 }
