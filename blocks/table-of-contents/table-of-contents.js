d15b3ee893c9a441c15fe4f04a52f5b15410abdb
Mirko Sagolj <msagolj@adobe.com>
2022-09-22 16:58:27 +0200

    lint fixes

--- blocks/table-of-contents/table-of-contents.js
+++ blocks/table-of-contents/table-of-contents.js
@@ -1,20 +1,20 @@
-export default function decorate(block){
-    
+export default function decorate(block) {
+
     let flexDiv = document.createElement('div');
 
     [...block.children].forEach(row => {
         let anchor = row.children[1].children[0].getAttribute('href')
         let title = row.children[0].innerHTML;
-        console.log(anchor,title);
+        console.log(anchor, title);
         let a = document.createElement('a');
-        a.setAttribute('class','tocButton');
-        a.setAttribute('href',anchor)
+        a.setAttribute('class', 'tocButton');
+        a.setAttribute('href', anchor)
         let span = document.createElement('span');
-        span.setAttribute('class',"tocTitle");
+        span.setAttribute('class', "tocTitle");
         span.innerText = title;
         a.append(span);
         flexDiv.append(a);
-    });          
+    });
 
     block.textContent = '';
     block.append(flexDiv)
