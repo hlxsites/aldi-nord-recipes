271fb70f8b7fc9b1428cfdd5119f48398c44a1aa
Mark J. Becker <herzog31@users.noreply.github.com>
2022-09-22 14:03:22 +0200

    Add completed footer

---
+++ blocks/newsletter/newsletter.js
@@ -0,0 +1,37 @@
+export default function decorate(block) {
+  const cols = [...block.firstElementChild.children];
+  if (cols.length === 2) {
+    const apps = cols[0];
+    const form = cols[1];
+    apps.classList.add('newsletter-apps');
+    form.classList.add('newsletter-form');
+
+    // Apps
+    const appContent = document.createElement('div');
+    appContent.classList.add('newsletter-apps-content');
+    const left = document.createElement('div');
+    const right = document.createElement('div');
+    appContent.appendChild(left);
+    appContent.appendChild(right);
+
+    // Move image to left
+    const appImageContainer = apps.querySelector('.newsletter-apps > p');
+    left.appendChild(appImageContainer.querySelector('picture'));
+    apps.removeChild(appImageContainer);
+
+    // Move remaining content to right
+    apps.childNodes.forEach((elem) => {
+      apps.removeChild(elem);
+      right.appendChild(elem);
+    });
+    apps.appendChild(appContent);
+
+    // Newsletter Form
+    const formContainer = document.createRange().createContextualFragment(`<form>
+      <label aria-hidden="false" for="email">E-Mail-Adresse eingeben</label>
+      <input type="email" name="email" placeholder="E-Mail-Adresse eingeben">
+      <button type="submit">ZUR ANMELDUNG</button>
+    </form>`);
+    form.appendChild(formContainer);
+  }
+}
