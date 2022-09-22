3a551b9745a3907cdc7ed6b8de2b86fe8d894410
Julien Ramboz <ramboz@adobe.com>
2022-09-22 17:38:54 +0200

    feat: add experient toggle

--- scripts/experiment-preview.js
+++ scripts/experiment-preview.js
@@ -28,6 +28,7 @@ const createVariant = (variantName, config) => {
   // this will retain other query params such as ?rum=on
   experimentURL.searchParams.set('experiment', `${config.id}/${variantName}`);
 
+  div.id = variantName;
   div.className = `hlx-variant${config.selectedVariant === variantName ? ' hlx-variant-selected' : ' '}`;
   div.innerHTML = `<div>
   <h5>${variantName}</h5>
@@ -39,47 +40,72 @@ const createVariant = (variantName, config) => {
   return (div);
 };
 
+function getExperimentVariantPopup(config) {
+  const popup = document.createElement('div');
+  popup.className = 'hlx-experiment hlx-popup hlx-hidden';
+  popup.innerHTML = `
+    <div class="hlx-popup-header">
+      <div>
+        <h4>${config.experimentName}</h4>
+        <div class="hlx-details">${config.status}, ${config.audience}</div>
+      </div>
+      <div>
+        <div class="hlx-button"><a href="${config.manifest}">Manifest</a></div>
+      </div>
+    </div>
+    <div class="hlx-variants"></div>`;
+
+  const variants = popup.querySelector('.hlx-variants');
+  config.variantNames.forEach((vname) => {
+    const variantDiv = createVariant(vname, config);
+    variants.append(variantDiv);
+  });
+  return popup;
+}
+
+function getExperimentVariantSelectorInSidekick(config) {
+  const div = document.createElement('div');
+  div.className = 'experiment dropdown';
+
+  const button = document.createElement('button');
+  button.className = 'dropdown-toggle';
+  button.innerHTML = 'Experiment';
+  if (toClassName(config.status) === 'active') {
+    button.style.backgroundColor = '#280';
+  }
+  div.append(button);
+
+  const container = document.querySelector('helix-sidekick').shadowRoot.querySelector('.feature-container');
+  container.prepend(div);
+
+  return div;
+}
+
 /**
  * Create Badge if a Page is enlisted in a Helix Experiment
  * @return {Object} returns a badge or empty string
  */
-async function getExperimentVariantSelector() {
-  const experiment = toClassName(getMetadata('experiment'));
-  if (!experiment) {
-    return null;
-  }
+function getExperimentVariantSelectorInOverlay(config) {
+  const button = document.createElement('button');
+  button.className = 'hlx-experiment hlx-badge';
+  button.classList.add(`hlx-experiment-status-${toClassName(config.status)}`);
+  button.innerHTML = 'Experiment <span class="hlx-open"></span>';
+  return button;
+}
 
-  const config = window.hlx && window.hlx.experiment;
-  if (!config) {
-    return null;
-  }
-
-  const div = document.createElement('div');
-  div.className = 'hlx-experiment hlx-badge';
-  div.classList.add(`hlx-experiment-status-${toClassName(config.status)}`);
-  div.innerHTML = `Experiment <span class="hlx-open"></span>
-    <div class="hlx-popup hlx-hidden">
-      <div class="hlx-popup-header">
-        <div>
-          <h4>${config.experimentName}</h4>
-          <div class="hlx-details">${config.status}, ${config.audience}</div>
-        </div>
-        <div>
-          <div class="hlx-button"><a href="${config.manifest}">Manifest</a></div>
-        </div>
-      </div>
-      <div class="hlx-variants"></div>
-    </div>`;
-  const popup = div.querySelector('.hlx-popup');
-
-  const variantMap = {};
-
-  div.addEventListener('click', async () => {
+function registerClickHandler(button, popup, config) {
+  button.addEventListener('click', async () => {
     popup.classList.toggle('hlx-hidden');
+
+    const sidekick = document.querySelector('helix-sidekick');
+    if (sidekick) {
+      const { x, width } = button.getBoundingClientRect();
+      popup.style.right = `${window.innerWidth - x - width - 40}px`;
+    }
 
     // the query is a bit slow, so I'm only fetching the results when the popup is opened
     const resultsURL = new URL('https://helix-pages.anywhere.run/helix-services/run-query@v2/rum-experiments');
-    resultsURL.searchParams.set('experiment', experiment);
+    resultsURL.searchParams.set('experiment', config.experimentName);
     if (window.hlx.sidekickConfig && window.hlx.sidekickConfig.host) {
       // restrict results to the production host, this also reduces query cost
       resultsURL.searchParams.set('domain', window.hlx.sidekickConfig.host);
@@ -90,7 +116,7 @@ async function getExperimentVariantSelector() {
       const nf = {
         format: (num) => `${Math.floor(num)}%`,
       };
-      const variant = variantMap[result.variant];
+      const variant = document.querySelector(`#${result.variant}`);
       if (variant) {
         const performance = variant.querySelector('.performance');
         performance.innerHTML = `
@@ -101,32 +127,41 @@ async function getExperimentVariantSelector() {
       }
     });
   });
-
-  const variants = div.querySelector('.hlx-variants');
-  config.variantNames.forEach((vname) => {
-    const variantDiv = createVariant(vname, config);
-    variants.append(variantDiv);
-    variantMap[vname] = variantDiv;
-  });
-
-  return div;
 }
 
 async function createPreviewOverlay() {
+  const experiment = toClassName(getMetadata('experiment'));
+  if (!experiment) {
+    return;
+  }
+
+  const config = window.hlx && window.hlx.experiment;
+  if (!config) {
+    return;
+  }
+
   const sidekick = document.querySelector('helix-sidekick');
 
   loadCSS('/styles/experiment-preview.css');
   const overlay = document.createElement('div');
   overlay.className = 'hlx-preview-overlay';
 
-  if (sidekick) {
-    overlay.classList.add('hlx-preview-overlay-sidekick');
+  const popup = getExperimentVariantPopup(config);
+  const button = sidekick
+    ? getExperimentVariantSelectorInSidekick(config)
+    : getExperimentVariantSelectorInOverlay(config);
+
+  if (!button || !popup) {
+    return;
   }
 
-  const variantSelector = await getExperimentVariantSelector();
-  if (variantSelector) {
-    overlay.append(variantSelector);
+  registerClickHandler(button, popup, config);
+  if (!sidekick) {
+    overlay.append(button);
+  } else {
+    overlay.classList.add('hlx-preview-overlay-sidekick');
   }
+  overlay.append(popup);
 
   document.body.append(overlay);
 }
